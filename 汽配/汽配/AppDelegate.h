863d1f2031dc783ed015f68bab723a32aae767e3
tianfeng pan <tianfengpan@xxoodeMacBook-Pro.local>
2017-01-17 18:48:17 +0800

    初始化项目

---
+++ 汽配/汽配/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  汽配
+//
+//  Created by tianfeng pan on 17/1/17.
+//  Copyright © 2017年 tianfeng pan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 汽配/汽配/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  汽配
+//
+//  Created by tianfeng pan on 17/1/17.
+//  Copyright © 2017年 tianfeng pan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
